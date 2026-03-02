fcf82577ab42a6226b72f698efeb8146ef92e628
Som-Kashyap <somkashyap0123@gmail.com>
2026-03-02 10:45:09 +0530

    Added text to BallBouncer game

--- BallBouncer/BallBouncer.cpp
+++ BallBouncer/BallBouncer.cpp
@@ -8,21 +8,41 @@ int main() {
 
     window.setFramerateLimit(60);
 
+	// Creating ball
     sf::CircleShape ball(20.f);
     ball.setFillColor(sf::Color::Red);
     ball.setPosition(400.f, 300.f);
 
     sf::Vector2f ballVelocity(5.f, 3.f);
+
+	// Maximum speed for the ball
 	float maxspeed = 10.f;
 
     float paddleWidth = 100.f;
     float paddleHeight = 20.f;
+
+	// Creating paddle
     sf::RectangleShape paddle(sf::Vector2f(paddleWidth, paddleHeight));
     paddle.setFillColor(sf::Color::Blue);
     // Place paddle centered horizontally near the bottom
     paddle.setPosition((windowWidth - paddleWidth) / 2.f, windowHeight - paddleHeight - 10.f);
 
+	// Paddle movement speed
     float paddleSpeed = 5.0f;
+
+	//loading font for text display
+	sf::Font font;
+    if (!font.loadFromFile("resources/arial.ttf")) {
+        std::cout << "FONT FAILED!\n";
+    }
+		
+	sf::Text text;
+    text.setFont(font);
+	text.setString("Use left and right arrow keys to move the paddle");
+    text.setCharacterSize(20);
+	text.setFillColor(sf::Color::White);
+	text.setPosition(10.f, 10.f);
+
 
     while (window.isOpen()) {
         sf::Event event;
@@ -63,13 +83,14 @@ int main() {
 
 			if (abs(ballVelocity.y) > maxspeed) {
                 
-				ballVelocity.y = maxspeed * (ballVelocity.y > 0 ? 1 : -1); // Cap the speed
+				ballVelocity.y = maxspeed * (ballVelocity.y > 0 ? 1 : -1); // Cap the speed and maintain direction
             }
         }
 
         window.clear(sf::Color::Black);
         window.draw(ball);
         window.draw(paddle);
+		window.draw(text);
         window.display();
     }
 
