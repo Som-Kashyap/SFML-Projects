34e55c5e76a7f78e0c14f55fae346b85418ab0e0
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-22 09:56:53 +0530

    fixed errors

--- AutoMovingBall/AutoMovingBall.cpp
+++ AutoMovingBall/AutoMovingBall.cpp
@@ -1,5 +1,7 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 
 int main() {
 
@@ -13,47 +15,45 @@ int main() {
 	ball.setFillColor(sf::Color::Red);
 	ball.setPosition(400.f, 300.f);
 
-	sf::RectangleShape paddle(sf::Vector2f(100.f, 20.f));
-	paddle.setFillColor(sf::Color::Blue);
-	paddle.setPosition(350.f, 570.f);
-	float paddleSpeed = 1.f;
+	float deltaTime = 0.f;
+	sf::Clock clock;
 
-	sf::Vector2f velocity(1.0f, 1.0f);
+	sf::Vector2f velocity(500.0f, 400.0f);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 
+		deltaTime = clock.restart().asSeconds();
+
 		while (window.pollEvent(event)) {
+
 			if (event.type == sf::Event::Closed) {
+
 				window.close();
+
 			}
 
 		}
 
-		ball.move(velocity);
+		ball.move(velocity * deltaTime);
+
 		if (ball.getPosition().x + 2 * radius >= screenWidth || ball.getPosition().x <= 0) {
+
 			velocity.x = -velocity.x;
+			ball.setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+
 		}
 		if (ball.getPosition().y + 2 * radius >= screenHeight || ball.getPosition().y <= 0) {
-			velocity.y = -velocity.y;
-		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-			paddle.move(-paddleSpeed, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-			paddle.move(paddleSpeed, 0.f);
-		}
-
-		if(ball.getGlobalBounds().intersects(paddle.getGlobalBounds())) {
 			velocity.y = -velocity.y;
+			ball.setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+
 		}
 
 
 		window.clear();
 		window.draw(ball);
-		window.draw(paddle);
 		window.display();
 
 	}
